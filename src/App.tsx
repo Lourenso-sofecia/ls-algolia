eb9a02a0b488bf773e5a9f5a1ea0bfb409df54c9
sofecia <lourenso.sofecia@gmail.com>
2023-05-17 13:06:25 +0100

    Add RefinamentList add

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,15 @@
 import algoliasearch from 'algoliasearch/lite';
-import {
-  Configure,
-  HierarchicalMenu,
-  Hits,
-  InstantSearch,
-  Pagination,
-} from 'react-instantsearch-hooks-web';
+import { InstantSearch, RefinementList } from 'react-instantsearch-hooks-web';
 
-import { Autocomplete, Hit } from './components';
-import {
-  INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES,
-  INSTANT_SEARCH_INDEX_NAME,
-} from './constants';
-import { Panel } from './widgets/Panel';
-
-import './App.css';
-
-function App() {
-  const searchClient = algoliasearch(
+const searchClient = algoliasearch(
     'latency',
     '6be0576ff61c053d5f9a3225e2a90f76'
   );
-
+function App() {
   return (
-    <div>
-      <InstantSearch
-        searchClient={searchClient}
-        indexName={INSTANT_SEARCH_INDEX_NAME}
-        routing
-      >
-        <header className="header">
-          <div className="header-wrapper wrapper">
-            <nav className="header-nav">
-              <a href="/">Home</a>
-            </nav>
-            <Autocomplete
-              searchClient={searchClient}
-              placeholder="Search products"
-              detachedMediaQuery="none"
-              openOnFocus
-            />
-          </div>
-        </header>
-
-        <Configure
-          attributesToSnippet={['name:7', 'description:15']}
-          snippetEllipsisText="…"
-        />
-        <div className="container wrapper">
-          <div>
-            <Panel header="Categories">
-              <HierarchicalMenu
-                attributes={INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES}
-              />
-            </Panel>
-          </div>
-          <div>
-            <Hits hitComponent={Hit} />
-            <Pagination />
-          </div>
-        </div>
-      </InstantSearch>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="instant_search">
+      <RefinementList attribute="brand" />
+    </InstantSearch>
   );
 }
-
 export default App;
--- src/widgets/Panel.tsx
+++ src/widgets/Panel.tsx
@@ -1,3 +1,7 @@
+import { RefinementList } from "react-instantsearch-hooks-web";
+
+import { useNumericMenu } from 'react-instantsearch-hooks-web';
+
 export function Panel({
   children,
   header,
@@ -6,10 +10,14 @@ export function Panel({
   header?: React.ReactNode;
   footer?: React.ReactNode;
 }>) {
+
+  //const { items, refine } = useNumericMenu(props);
   return (
     <div className="ais-Panel">
       {header && <div className="ais-Panel-header">{header}</div>}
       <div className="ais-Panel-body">{children}</div>
+      <div className="ais-Panel-header">Brands. Search Here...</div>
+      <RefinementList attribute="brand" />
       {footer && <div className="ais-Panel-footer">{footer}</div>}
     </div>
   );
